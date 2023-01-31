9b6e986c32b122d0fcb6235a466a6bfa6f2597a3
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-01-31 17:08:06 +0100

    first commit

---
+++ 03-loops-and-arrays/reviews.js
@@ -0,0 +1,8 @@
+var reviews = [5, 5, 4.5, 1, 3];
+var total = 0;
+for (var i = 0; i < reviews.length; i++) {
+    console.log(reviews[i]);
+    total += reviews[i];
+}
+var average = total / reviews.length;
+console.log("Review average = ".concat(average));
