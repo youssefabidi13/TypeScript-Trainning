ed84a571bb45a0e1692a97a9a495d0357bd247fd
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-01-31 16:45:32 +0100

    first commit

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Youssef";
+var lastName = "Abidi";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: String = "Youssef";
+let lastName: String = "Abidi";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName +" "+lastName);
+console.log(`Hi ${firstName} ${lastName}`);
